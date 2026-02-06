48b061a46ee471b06e3f3e0c03b8b35e03d8ad7b
tobias <tobiasceniceros@gmail.com>
2026-02-06 08:54:56 +0100

    Menu pausa fix

--- juego.gd
+++ juego.gd
@@ -4,7 +4,8 @@ class_name Juego extends Node
 var player = load("res://player/main_character.tscn")
 
 func _ready() -> void:
-	_menu_pausa.open()
+	pass
+	#_menu_pausa.open()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"Pause"):
